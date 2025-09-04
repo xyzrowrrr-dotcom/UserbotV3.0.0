b4b99b60fa6170757c4c4749e8e637710f933ae2
JunHosting <junofficial354@gmail.com>
2025-09-04 23:10:54 +0700

    update biar simple

--- settings.py
+++ settings.py
@@ -4,12 +4,12 @@ OWNER_NAME = "JUN NOT DEVS"
 GROUP_CHAT = "junofficial354gc"
 CHANNEL = "junofficial354ch"
 URL_ADMIN = "https://t.me/junofficial354bot"
-IP = "IP BOKEP INI JANGAN DI GANTI 😹"
+IP = "152.42.167.204"
 # === BOT INFO ===
-BOT_NAME = "TIKUS KANTOR DPR BABI"
-PP = "https://files.catbox.moe/zzx0s5.jpg" # foto bokep ini bebas 🗿
+BOT_NAME = "UBOT MMK"
+PP = "https://files.catbox.moe/zzx0s5.jpg"
 BUG_URL = "https://files.catbox.moe/zzx0s5.jpg"
-BOT_TOKEN = "BERHARAP_ADA_TOKENNYA_KAH?"
+BOT_TOKEN = "8458392296:AAGt5_OhAN_CViN-CH-gqRRPa-4SIx14DPY"
 # === PTERODACTYL PANEL ===
 DOMAIN = "https://junaoffc.panelmurmer.web.id"
 PLTA = "ptla_ZeiAUwnqme299jhutwvbKYpOS0AWDUORRKJQtG4qbdR"
@@ -18,8 +18,8 @@ LOC = 1
 EGGS = 15
 
 # === TELEGRAM API (my.telegram.org) ===
-API_ID = 999999
-API_HASH = "XNXXBOKEPGACOR123"
+API_ID = 23205651
+API_HASH = "5a5d93f4564092db12126729cacf58ac"
 
 
 # apa ga bisa ambil api hash ama api id? A N A K  P I N T A R 😊
